62ca51093c1de97e877d9195108b3e4f9d60c57f
Rafael Gomes <rafaelgomesdasilva.faculdade@gmail.com>
2025-09-16 20:02:50 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App {
+
+public static void main(String[] args) {
+    String salaDesejada = "A219";
+    String salaInformada = "";
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println(x:"Digite o número da sala: ");
+    salaInformada = scanner.next();
+    
+    if (salaDesejada.equls(salaInformada))
+
+    System.out.println(x: "VocÊ encontrou sua sala!");
+    
+} else {
+    System.out.println(x: "Você está perdido!\nSe enontre!");
+    
+}
+
+scanner.close ();
